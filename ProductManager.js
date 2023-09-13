a22e69f9f6601c0e317327a0cb681a0b127a0d09
Armando Rodriguez <armandorodriguezm1998@gmail.com>
2023-09-12 21:54:48 -0600

    arreglos

--- ProductManager.js
+++ ProductManager.js
@@ -29,7 +29,7 @@ class ProductManager {
     getProductById(id) {
         const product = this.products.find(product => product.id === id);
         if (!product) {
-            console.error('Producto no encontrado', "\n");
+            console.error('Producto no encontrado');
         }
         return product;
     }
@@ -40,12 +40,12 @@ class ProductManager {
 
     addProduct(title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
-            console.error('Todos los campos son requeridos', "\n");
+            console.error('Todos los campos son requeridos');
             return;
         }
 
         if (this.products.some(product => product.code === code)) {
-            console.error('El producto con el mismo código ya existe', "\n");
+            console.error('El producto con el mismo código ya existe');
             return;
         }
 
@@ -67,38 +67,40 @@ class ProductManager {
     updateProduct(id, updatedProduct) {
         const productIndex = this.products.findIndex(product => product.id === id);
         if (productIndex === -1) {
-            console.error('Producto no encontrado', "\n");
+            console.error('Producto no encontrado');
             return;
         }
 
         this.products[productIndex] = { ...updatedProduct, id };
         this.saveProductsToFile();
-        console.log('Producto actualizado satisfactoriamente:', this.products[productIndex], "\n");
+        console.log('Producto actualizado satisfactoriamente:', this.products[productIndex]);
     }
 
     deleteProduct(id) {
         const productIndex = this.products.findIndex(product => product.id === id);
         if (productIndex === -1) {
-            console.error('Producto no encontrado', "\n");
+            console.error('Producto no encontrado');
             return;
         }
 
         this.products.splice(productIndex, 1);
         this.saveProductsToFile();
-        console.log('Producto eliminado satisfactoriamente', "\n");
+        console.log('Producto eliminado satisfactoriamente');
     }
 }
 
 // Ejemplo de uso:
-const manager = new ProductManager('ProductsFile.json');
+const manager = new ProductManager('ProductsFile.json'); //Clase manager
 
-manager.addProduct('Teclado', 'RGB mecánico', 250.00, 'https://m.media-amazon.com/images/I/61CJJ297IJL._AC_SL1200_.jpg', 'P1', 8);
-manager.addProduct('Mouse', 'RGB inalámbrico', 450.50, 'https://m.media-amazon.com/images/I/6177U5fDSwL.__AC_SX300_SY300_QL70_ML2_.jpg', 'P2', 5);
+manager.addProduct('Teclado', 'RGB mecánico', 250.00, 'https://m.media-amazon.com/images/I/61CJJ297IJL._AC_SL1200_.jpg', 'P1', 8); //Primer producto a agregar
+manager.addProduct('Mouse', 'RGB inalámbrico', 450.50, 'https://m.media-amazon.com/images/I/6177U5fDSwL.__AC_SX300_SY300_QL70_ML2_.jpg', 'P2', 5); //Segundo producto a agregar
 
 const allProducts = manager.getProducts();
-console.log('Todos los productos:', allProducts, "\n");
+console.log('Todos los productos:', allProducts); //
 
 const productById = manager.getProductById(1); // Mostrar primer producto
-console.log('Productos por ID:', productById, "\n");
+console.log('Productos por ID:', productById);
+
+const deleteById = manager.deleteProduct(1); // Eliminar primer producto
 
 const nonExistingProduct = manager.getProductById(3); // Mostrar el mensaje de error
